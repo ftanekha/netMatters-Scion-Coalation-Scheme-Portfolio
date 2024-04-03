47ce33c27ddd7577d004ba4a804f1e5c296bd966
ftanekha <farai.tanekha@gmail.com>
2024-04-03 13:44:08 +0100

    nav#main-nav - add menu/ hamburger icon

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 document.addEventListener(
     'DOMContentLoaded',
     ()=>{
-        console.log( document.URL)
+        //toggle nav menu
+        const menuIcon = document.querySelector('.icon-menu')
+        menuIcon.addEventListener(
+            'click',
+            ()=> {
+                let menu = document.querySelector('.icon-menu + ul')
+                let menuDisplayMode = document.querySelector('.icon-menu + ul').style.display
+                if(menuDisplayMode =='none'){
+                    menu.style.display = 'flex'
+                }else{
+                    menu.style.display = 'none'  
+                }
+            }
+        )
     }
 )
