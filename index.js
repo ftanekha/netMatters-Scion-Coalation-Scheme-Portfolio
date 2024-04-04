e46783e2fb4cf4da28052bba113a72228bfdd9bc
ftanekha <farai.tanekha@gmail.com>
2024-04-04 01:45:28 +0100

    SPLIT ALL SECTIONS into individual pages  Main changes:  - add main navigation menu icon  - remove navigation list background on bg screens  - make all main components responsive  - give all main sections min-height == full page  >>  #homepage  - add hover effect to arrow-down-icon
     #about-me
     - adjust all font-sizes to fit the full page layout
     - adjust the social media icons' size accordingly
    
     #my-portfolio
     - make all text-align == justify
     - change the border-color of the 'dl' els to match the overall theme
     - resize project-link images to a more comfortable size
    
     #code-examples
     - remove the big orange background, it's too visible and dull
     - restyle the construction site cone to align with the nav menu icon
     - improve the spacing btwn the flexbox items
    
     #SCS
     - minor cleanup
    
     #cantact-me
     - resize the form, make it mpre appreciable on bg screen sizes
     - minor cleanup
    
     #nav-menu
     - add hamburger icon
     - add homepage link
     - add responsiveness

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 document.addEventListener(
     'DOMContentLoaded',
     ()=>{
-        //toggle nav menu
+        function toggleMenu(){
+            // show/hide nav menu items/links
+            let menu = document.querySelector('.icon-menu + ul')
+                let menuDisplayMode = menu.style.display
+                if(menuDisplayMode == 'none'){
+                    with(menu.style){
+                        display = 'flex'
+                        flexDirection = 'column'
+                        listStyleType = 'none'
+                    }
+                }else{
+                    menu.style.display = 'none'  
+                }
+        }
+        //toggleMenu with menu icon
         const menuIcon = document.querySelector('.icon-menu')
         menuIcon.addEventListener(
             'click',
             ()=> {
-                let menu = document.querySelector('.icon-menu + ul')
-                let menuDisplayMode = document.querySelector('.icon-menu + ul').style.display
-                if(menuDisplayMode =='none'){
-                    menu.style.display = 'flex'
-                }else{
-                    menu.style.display = 'none'  
-                }
+                toggleMenu()
+            }
+        )
+        // toggleMenu with nav menu item
+        const menuItem = document.querySelector('.nav')
+        menuItem.addEventListener(
+            'click',
+            ({target})=> {
+                target.parentNode.style.display = 'none'
+                toggleMenu()
             }
         )
     }
