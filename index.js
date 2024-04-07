3cc75b0dfaedf1529601c7f58d9554d7d0cdf7d7
ftanekha <farai.tanekha@gmail.com>
2024-04-07 19:19:15 +0100

    Group together and Initiate homepage functionlities

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 document.addEventListener(
     'DOMContentLoaded',
     ()=>{
-        function toggleMenu(){
+        //INITIATE HOMEPAGE FUNCTIONALITIES
+        function initiateHomepageFunctionality(){
             //show/hide nav menu items/links
-            const menu = document.querySelector('nav > ul')
-            with(menu.style){
-                if(display != 'flex'){
-                    display = 'flex'
-                    flexDirection = 'column'
-                    listStyleType = 'none'
-                }else{
-                    display = 'none'
+            function toggleMenu(){
+                const menu = document.querySelector('nav > ul')
+                with(menu.style){
+                    if(display != 'flex'){
+                        display = 'flex'
+                        flexDirection = 'column'
+                        listStyleType = 'none'
+                    }else{
+                        display = 'none'
+                    }
+                }     
+            }
+            //animate homepage arrow-down icon 
+            function animateArrowDown(){
+                const arrowDown = document.querySelector('.icon-arrow-down')
+                with(arrowDown.style){
+                    animationName = 'arrow-pulse'
+                    animationDuration = 6 + 's'
                 }
-            }     
-        }
-        //animate homepage arrow-down icon 
-        function animateArrowDown(){
-            const arrowDown = document.querySelector('.icon-arrow-down')
-            with(arrowDown.style){
-                animationName = 'arrow-pulse'
-                animationDuration = 6 + 's'
             }
-        }
-        function isHomePage(){
-            const path = document.location.pathname
-            if(
-                path == '/'
-                        ||
-                path == '/netMatters-Scion-Coalation-Scheme-Portfolio/index.html' 
-                        || 
-                path == '/index.html'
-            ){
-                //play animation on initial page load
-                animateArrowDown()
-            }else{
-                //only toggle menu where it exists
-                document?.querySelector('.icon-menu')?.addEventListener(
-                    'click',
-                    toggleMenu
-                )
+            function isHomePage(){
+                const path = document.location.pathname
+                if(
+                    path == '/'
+                            ||
+                    path == '/netMatters-Scion-Coalation-Scheme-Portfolio/index.html' 
+                            || 
+                    path == '/index.html'
+                ){
+                    //play animation on initial homepage load
+                    animateArrowDown()
+                }else{
+                    //only toggle menu where it exists i.e. homepage
+                    document?.querySelector('.icon-menu')?.addEventListener(
+                        'click',
+                        toggleMenu
+                    )
+                }
             }
+            isHomePage()
         }
-        isHomePage()
+        initiateHomepageFunctionality()
     }
 )
