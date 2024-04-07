205ab7d5b5777cb9d056003f2909a244b75e1345
ftanekha <farai.tanekha@gmail.com>
2024-04-08 00:43:42 +0100

    apply temporary to page freezing when not on homepage, ref index.js => displayInitials()

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.addEventListener(
     ()=>{
         //INITIATE HOMEPAGE FUNCTIONALITIES
         function initiateHomepageFunctionality(){
+            //current URL pathname
+            const path = document.location.pathname
             //show/hide nav menu items/links
             function toggleMenu(){
                 const menu = document.querySelector('nav > ul')
@@ -11,44 +13,61 @@ document.addEventListener(
                         display = 'flex'
                         flexDirection = 'column'
                         listStyleType = 'none'
-                    }else{
-                        display = 'none'
-                    }
-                }     
-            }
-            //animate homepage arrow-down icon 
-            function animateArrowDown(){
-                const arrowDown = document.querySelector('.icon-arrow-down')
-                with(arrowDown.style){
-                    animationName = 'arrow-pulse'
-                    animationDuration = 6 + 's'
+                    //    console.log(menu.firstElementChild)
+                        // highlight current page in nav menu
+                        // determine current page link name
+
+
+                        // const urlExtension = '.html'
+                        // const currentPageLink = path.substring(1, path.length - urlExtension.length)
+                        // console.log(currentPageLink)
+                        // //target current page link item
+                        // const currentPageNavListItem = document.querySelector(`.item-${currentPageLink}`)
+                        // console.log(currentPageNavListItem.firstElementChild)
+
+
+                        // highlight the nested anchor element
+                        // currentPageNavListItem.style.backgroundColor = 'black'
+                        //address problematic cases
+                        // setTimeout(() => {
+                        //     switch(currentPageLink){
+                        //         case 'my-portfolio':
+                        //             (document.querySelector('.item-my-portfolio a')).style.backgroundColor = 'black'
+                        //             break
+                        //         case 'scs':
+                        //             (document.querySelector('.item-scs a')).style.backgroundColor = 'black'
+                        //             break
+                        //         default:
+                        //             console.error('Error: Invalink nav-menu link!')
+                        //             break
+                        //     }
+                        // }, 3e3);
+                    }     
                 }
-            }
-            function isHomePage(){
-                const path = document.location.pathname
-                if(
-                    path == '/'
-                            ||
-                    path == '/netMatters-Scion-Coalation-Scheme-Portfolio/index.html' 
-                            || 
-                    path == '/index.html'
-                ){
-                    //play animation on initial homepage load
+                //animate homepage arrow-down icon 
+                function animateArrowDown(){
+                    const arrowDown = document.querySelector('.icon-arrow-down')
+                    with(arrowDown.style){
+                        animationName = 'arrow-pulse'
+                        animationDuration = 6 + 's'
+                    }
+                }
+                //play animation on initial homepage load
+                if( isHomePage() ){
                     animateArrowDown()
-                }else{
-                    //only toggle menu where it exists i.e. homepage
-                    document?.querySelector('.icon-menu')?.addEventListener(
+                }else{//OR
+                    //only toggle menu where it exists i.e. NOT homepage
+                    document.querySelector('.icon-menu').addEventListener(
                         'click',
                         toggleMenu
                     )
                 }
             }
-            isHomePage()
         }
         initiateHomepageFunctionality()
         //////
         //DISPLAY RANDOM INITIALS
-        function displayRandonInitials(){
+        function displayInitials(){
             const targetImage = document.querySelector('#initials-image')
             const imgSourceOptions = 
             [ 
@@ -63,9 +82,23 @@ document.addEventListener(
                 return Math.floor(Math.random() * (rangeFactor + 1)) + min
             }
             const randomIndex = generateRandomImageSource(imgSourceOptions)
-            return targetImage.style.mixBlendMode = imgSourceOptions[randomIndex]
+            console.log(randomIndex)
+            // return targetImage.style.mixBlendMode = imgSourceOptions[randomIndex]
+            try{
+                return targetImage.style.mixBlendMode = imgSourceOptions[randomIndex]
+            }catch(err){
+                const smilyFace = '\uD83D\uDE38'
+                if(err.name == 'TypeError') console.warn(`Minor error: ${err.name}. ANTICIPATED as current page NOT homepage ${smilyFace}`)
+                //temporary solution as page freezes
+                const timeout = setTimeout(
+                    ()=> {
+                        location.assign('/index.html')
+                        clearTimeout(timeout)
+                    }, 3e3
+                )
+            }
         }
-        displayRandonInitials()
+        displayInitials()
         //////
     }
 )
