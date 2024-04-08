13aa151e4d3cbfb447255fcf040cf0afd320a9b7
ftanekha <farai.tanekha@gmail.com>
2024-04-08 03:29:16 +0100

    nav#main-nav >>menu-icon - reduce menu icon size, padding, and margin on small screen devices - rotate(90deg) to utilise space efficiently

--- index.js
+++ index.js
@@ -1,19 +1,78 @@
 document.addEventListener(
     'DOMContentLoaded',
     ()=>{
+        //current URL pathname
+        const path = document.location.pathname
+        function isHomePage(){
+            if(
+                path == '/'
+                        ||
+                path == '/netMatters-Scion-Coalation-Scheme-Portfolio/index.html' 
+                        || 
+                path == '/index.html'
+            )return true
+            return false
+        }
         //INITIATE HOMEPAGE FUNCTIONALITIES
         function initiateHomepageFunctionality(){
-            //current URL pathname
-            const path = document.location.pathname
-            //show/hide nav menu items/links
-            function toggleMenu(){
-                const menu = document.querySelector('nav > ul')
-                with(menu.style){
-                    if(display != 'flex'){
-                        display = 'flex'
-                        flexDirection = 'column'
-                        listStyleType = 'none'
-                    //    console.log(menu.firstElementChild)
+            //animate homepage arrow-down icon 
+            function animateArrowDown(){
+                const arrowDown = document.querySelector('.icon-arrow-down')
+                with(arrowDown.style){
+                    animationName = 'arrow-pulse'
+                    animationDuration = 6 + 's'
+                }
+            }
+            //DISPLAY RANDOM INITIALS
+            function displayInitials(){
+                const targetImage = document.querySelector('#initials-image')
+                const imgSourceOptions = 
+                [ 
+                    "multiply", "hard-light",
+                    "difference", "exclusion",
+                    "plus-darker", "plus-lighter"
+                ]
+                // generate random blend mode on page load
+                function generateRandomImageBlendMode(optionsArray){
+                    const [min, max] = [0, optionsArray.length - 1]
+                    const rangeFactor = max - min
+                    return Math.floor(Math.random() * (rangeFactor + 1)) + min
+                }
+                const randomIndex = generateRandomImageBlendMode(imgSourceOptions)
+                //display initials in randomised blend mode
+                targetImage.style.mixBlendMode = imgSourceOptions[randomIndex]
+            }
+            //
+            animateArrowDown()
+            displayInitials()
+        }
+        //show/hide nav menu items/links
+        function toggleMenu(){
+            const menu = document.querySelector('nav > ul')
+            with(menu.style){
+                if(display != 'flex'){
+                    display = 'flex'
+                    flexDirection = 'column'
+                    listStyleType = 'none'
+                } else{
+                    display = 'none'
+                }    
+            }
+        }
+        //animate menu icon & display initials on initial homepage load
+        if( isHomePage() ){
+            initiateHomepageFunctionality()
+        }else{//OR
+            //only toggle menu where it exists i.e. NOT homepage
+            document.querySelector('.icon-menu').addEventListener(
+                'click',
+                toggleMenu
+            )
+        }
+    }
+)
+
+//    console.log(menu.firstElementChild)
                         // highlight current page in nav menu
                         // determine current page link name
 
@@ -42,63 +101,3 @@ document.addEventListener(
                         //             break
                         //     }
                         // }, 3e3);
-                    }     
-                }
-                //animate homepage arrow-down icon 
-                function animateArrowDown(){
-                    const arrowDown = document.querySelector('.icon-arrow-down')
-                    with(arrowDown.style){
-                        animationName = 'arrow-pulse'
-                        animationDuration = 6 + 's'
-                    }
-                }
-                //play animation on initial homepage load
-                if( isHomePage() ){
-                    animateArrowDown()
-                }else{//OR
-                    //only toggle menu where it exists i.e. NOT homepage
-                    document.querySelector('.icon-menu').addEventListener(
-                        'click',
-                        toggleMenu
-                    )
-                }
-            }
-        }
-        initiateHomepageFunctionality()
-        //////
-        //DISPLAY RANDOM INITIALS
-        function displayInitials(){
-            const targetImage = document.querySelector('#initials-image')
-            const imgSourceOptions = 
-            [ 
-                "multiply", "hard-light",
-                "difference", "exclusion",
-                "plus-darker", "plus-lighter"
-            ]
-            // generate random source on page load
-            function generateRandomImageSource(optionsArray){
-                const [min, max] = [0, optionsArray.length - 1]
-                const rangeFactor = max - min
-                return Math.floor(Math.random() * (rangeFactor + 1)) + min
-            }
-            const randomIndex = generateRandomImageSource(imgSourceOptions)
-            console.log(randomIndex)
-            // return targetImage.style.mixBlendMode = imgSourceOptions[randomIndex]
-            try{
-                return targetImage.style.mixBlendMode = imgSourceOptions[randomIndex]
-            }catch(err){
-                const smilyFace = '\uD83D\uDE38'
-                if(err.name == 'TypeError') console.warn(`Minor error: ${err.name}. ANTICIPATED as current page NOT homepage ${smilyFace}`)
-                //temporary solution as page freezes
-                const timeout = setTimeout(
-                    ()=> {
-                        location.assign('/index.html')
-                        clearTimeout(timeout)
-                    }, 3e3
-                )
-            }
-        }
-        displayInitials()
-        //////
-    }
-)
