6a282b88fb8864ea3b1b3cd85921d77d164ab7f8
ftanekha <farai.tanekha@gmail.com>
2024-06-10 04:28:04 +0100

    -add jquery animations to homepage

---
+++ animations.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+    // display my name
+    $('#banner big').css('display', 'none').delay(2000).fadeIn(5000)
+    //display main nav list items
+    $('nav li').css('display', 'none').delay(2000).slideDown(2000)
+    //display scroll down arrow
+    $('.icon-arrow-down').css('display', 'none').delay(2000).fadeIn(4000)
+})
